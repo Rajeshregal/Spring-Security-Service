e911a3f1ce2b261858490b4c71f5db1e2b5dd649
rajesh <alwaysrajesh946@gmail.com>
2026-04-24 19:18:22 +0530

    test change01

---
+++ src/main/java/com/example/security/util/test.java
@@ -0,0 +1,13 @@
+package com.example.security.util;
+
+public class test {
+    public void m1(){
+        System.out.println("m1 method");
+    }
+     public void m2(){
+        System.out.println("m2 method");
+    }
+     public void m3(){
+        System.out.println("m3 method");
+    }
+}
