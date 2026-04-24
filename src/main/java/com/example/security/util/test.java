e6db68c7bf5b5f71e9500c6bbc44ccaf7ec2bc59
rajesh <alwaysrajesh946@gmail.com>
2026-04-24 19:48:10 +0530

    chnages from branch02

--- src/main/java/com/example/security/util/test.java
+++ src/main/java/com/example/security/util/test.java
@@ -2,7 +2,9 @@ package com.example.security.util;
 
 public class test {
     public void m1(){
-        System.out.println("m1 method");
+        System.out.println("Hey I am from branch 2");
+        System.out.println("m1 method Updated by branch 2");
+        System.out.println("Hey I am from branch 2");
     }
      public void m2(){
         System.out.println("m2 method");
